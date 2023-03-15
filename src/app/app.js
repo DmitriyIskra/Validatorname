90c240952ce47abcfc7d49027c4bc2588df3a47d
Dmitriy Iskra <dmitriyiskra@mail.ru>
2023-03-15 23:46:50 +0300

    initial commit

---
+++ src/app/app.js
@@ -0,0 +1,21 @@
+export default class Validator {
+  constructor() {
+    this.validateStart = /^[^\d_-]/;
+    this.validateEnd = /[^\d_-]$/;
+    this.validateThreeNum = /\d{4,}/;
+    this.validateText = /^[\w-]*$/i;
+  }
+
+  validateUsername(name) {
+    const resultText = this.validateText.test(name);
+    const resultStart = this.validateStart.test(name);
+    const resultEnd = this.validateStart.test(name);
+    const resultThreeNum = this.validateThreeNum.test(name);
+
+    return !!(resultStart && resultText && !resultThreeNum && resultEnd);
+  }
+}
+
+// const validator = new Validator()
+
+// console.log(validator.validateUsername('cbfg_545df'))
